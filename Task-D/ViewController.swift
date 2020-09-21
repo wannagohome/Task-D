443ca3d2c90eafe8439f2af2283d00d437642989
wannagohome <iwwuf7@gmail.com>
2020-09-21 20:18:47 +0900

    Initial Commit

---
+++ Task-D/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Task-D
+//
+//  Created by jinho jang on 2020/09/21.
+//  Copyright © 2020 Pete. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
